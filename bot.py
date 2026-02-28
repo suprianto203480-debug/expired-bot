82bfc5769b71d13f5c2f190d7a50631ef2e8c7d0
suprianto203480-debug <suprianto203480@gmail.com>
2026-03-01 06:37:54 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -580,7 +580,7 @@ if __name__ == "__main__":
     app.add_handler(MessageHandler(filters.Regex("^ℹ️ Help$"), help_menu))
     app.add_handler(MessageHandler(filters.Regex("^🏠 Menu Utama$"), menu_utama))
 
-    if __name__ == "__main__":
+ if __name__ == "__main__":
     print("✅ BOT FINAL STABLE RUNNING")
     check_column_type()
     app.run_polling()
